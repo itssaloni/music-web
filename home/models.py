df010d63ad304d36c1cad5dbceb5be7e4f9d9560
Saloni <saloni.pal015@gmail.com>
2023-10-29 20:12:28 +0530

    Audio

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+class Audio(models.Model):
+    file = models.FileField(upload_to='audio/')
+    user = models.ForeignKey(User, on_delete= models.CASCADE)
+    thumbnail = models.ImageField(upload_to= 'thumbnail/', blank= True, null = True)
+    likes = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.file.url
+    
